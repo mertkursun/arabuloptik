4b71d233684b455b4104131318b95ea15b5179da
Mustafa Mert Kursun <mustafamertkursun@Mustafa-MacBook-Pro.local>
2020-05-10 00:27:42 +0300

    commıt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Arabul Optik
+        <img src={"./banner.jpg"} className="" alt="logo" />
+        <p className="name">
+          ARABUL OPTİK
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="content">
+          Yeni hayatın normallerini oluşturduğumuz bu dönemde çok yakında yeni arayüzümüz ile web sitemiz yayında olacak...
+        </div>
+        <div className="address">
+          <p>Şehremini Mah. Mimar Kasım Cad. 61/A <br/>Fatih/İSTANBUL</p>
+          <p className="tel">0212 529 85 72</p>
+        </div>
       </header>
     </div>
   );
